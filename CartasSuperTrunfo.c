8c71666fa3d4775e421458b833b0c5aecab71179
welintonvieira1-create <welinton.vieira1@gmail.com>
2026-03-03 12:32:59 -0300

    Initialize city cards with predefined data

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,13 +6,23 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-    char estado1[50],estado2[50];
-    char codigo1[50],codigo2[50];
-    char nome1[50],nome2[50];
-    int populacao1,populacao2;
-    float area1,area2;
-    float pib1,pib2;
-    int pontosturisticos1,pontosturisticos2;
+  //carta 1 
+    char estado1[50] = "ceara";
+    char codigo1[50] = "A01";
+    char nome1[50] = "fortaleza";
+    int populacao1 = 294574;
+    float area1 = 314.93;
+    float pib1 = 42305985.50;
+    int pontosturisticos1 = 15
+  
+// Carta2
+  char estado2[50] = "ceara";
+    char codigo2[50] = "A01";
+    char nome2[50] = "fortaleza";
+    int populacao2 = 294574;
+    float area2 = 314.93;
+    float pib2 = 42305985.50;
+    int pontosturisticos2 = 15
   // Área para entrada de dados
     printf("Digite o nome do primeiro estado: \n");
     scanf("%s", estado1);  
@@ -56,8 +66,8 @@ int main() {
     printf("Digite o numero de pontos turisticos do segundo estado: \n");
     scanf("%d", &pontosturisticos2);
   // Área para exibição dos dados da cidade
-    printf("Dados do primeiro estado:\n");
-    printf("Nome: %s\n", estado1);
+    printf("Dados do primeira Carta:\n");
+    printf("Estado: %s\n", estado1);
     printf("Codigo: %s\n", codigo1);
     printf("Cidade: %s\n", nome1);
     printf("Populacao: %d\n", populacao1);
@@ -65,8 +75,8 @@ int main() {
     printf("PIB: %.2f Bilhoes de reais\n", pib1);
     printf("Pontos Turisticos: %d\n", pontosturisticos1);
 
-    printf("Dados do segundo estado:\n");
-    printf("Nome: %s\n", estado2);
+    printf("Dados do segunda carta:\n");
+    printf("Estado: %s\n", estado2);
     printf("Codigo: %s\n", codigo2);
     printf("Cidade: %s\n", nome2);
     printf("Populacao: %d\n", populacao2);
